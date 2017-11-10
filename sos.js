327269133904cf78a03696ea4281a7ccd6960d01
Rikke Vestergaard <rikkevestergaard@Rikkes-MacBook-Pro.local>
2017-11-10 12:37:50 +0100

    fugl

--- sos.js
+++ sos.js
@@ -1,21 +1,26 @@
+$(window).on("load", startHistorien);
 /// START ///
-function vingebask_sound() {
-
-
+function startHistorien() {
+    console.log("startHistorien");
+    $("#vingebask_sound")[0].play();
+    $("#fugl_container").removeClass("fugl_start_pos");
+    $("#fugl_container").addClass("fugl_flyver");
+    $("#fugl_sprite").addClass("fugl_flyvecycle");
+    $("#startknap").hide();
+    $("#fugl_container").on("animationend", fuglenErInde);
 }
 
-$("#startknap").on("click", start_f);
-
-// Intro funktioner
-
+function fuglenErInde() {
+    $("#startknap").show();
+    $("#startknap").on("click", start_f);
+}
+// Intro funktioner //
 function start_f() {
     console.log("Siden er nu loadet.");
-    // Remove:
-    $("#startknap").hide();
-    $("#fugl").css("animation-play-state", "running");
-    $("#vingebask_sound")[0].play();
-    $("#fugl").on("animationend", eva_ind);
     $("#startknap").off("click", start_f);
+    // Remove
+    $("#startknap").hide();
+    $("#fugl_container").on("animationend", eva_ind);
 }
 
 function eva_ind() {
